aee07c0521ba36b6d197427357ccff6132f71f07
Marta Waplak <mwaplak@gmail.com>
2018-05-18 19:15:45 +0200

    Database link update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://<Solitary82>:<Altergothic1>@ds227740.mlab.com:27740/nodeappdatabase', {
     useMongoClient: true
 });
 
