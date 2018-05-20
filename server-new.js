3536b9a578e0d5c03917514164e8cc7f63d5c6b6
Marta Waplak <mwaplak@gmail.com>
2018-05-20 15:04:30 +0200

    Edit server-new.js

--- server-new.js
+++ server-new.js
@@ -13,7 +13,7 @@ app.listen(PORT, () => console.log('Example app listening on port ' + PORT));
 
 
 mongoose.Promise = global.Promise;
-;
+mongoose.connect('mongodb://Solitary82:Altergothic1@ds227740.mlab.com:27740/nodeappdatabase');
 
 const userSchema = new Schema({
   name: String,
@@ -42,108 +42,3 @@ const User = mongoose.model('User', userSchema),
     username: 'Kenny_the_boy',
     password: 'password'
   });
-
-kenny.manify(function(err, name) {
-  if (err) throw err;
-  console.log('Twoje nowe imię to: ' + name);
-});
-
-const benny = new User({
-  name: 'Benny',
-  username: 'Benny_the_boy',
-  password: 'password'
-});
-
-benny.manify(function(err, name) {
-  if (err) throw err;
-  console.log('Twoje nowe imię to: ' + name);
-});
-
-const mark = new User({
-  name: 'Mark',
-  username: 'Mark_the_boy',
-  password: 'password'
-});
-
-mark.manify(function(err, name) {
-  if (err) throw err;
-  console.log('Twoje nowe imię to: ' + name);
-});
-
-const findAllUsers = function() {
-  return User.find({}, function(err, res) {
-    if (err) throw err;
-    console.log('Actual database records are ' + res);
-  });
-};
-
-const findSpecificRecord = function() {
-  return User.find({ username: 'Kenny_the_boy' }, function(err, res) {
-    if (err) throw err;
-    console.log('Record you are looking for is ' + res);
-  });
-};
-
-const updateUserPassword = function() {
-  return User.findOne({ username: 'Kenny_the_boy' }).then(function(user) {
-    console.log('Old password is ' + user.password);
-    console.log('Name ' + user.name);
-    user.password = 'newPassword';
-    console.log('New password is ' + user.password);
-    return user.save(function(err) {
-      if (err) throw err;
-      console.log(
-        'Uzytkownik ' + user.name + ' zostal pomyslnie zaktualizowany'
-      );
-    });
-  });
-};
-
-const updateUsername = function() {
-  return User.findOneAndUpdate(
-    { username: 'Benny_the_boy' },
-    { username: 'Benny_the_man' },
-    { new: true },
-    function(err, user) {
-      if (err) throw err;
-      console.log('Nazwa uzytkownika po aktualizacji to ' + user.username);
-    }
-  );
-};
-
-const findMarkAndDelete = function() {
-  return User.findOne({ username: 'Mark_the_boy' }).then(function(user) {
-    return user.remove(function() {
-      console.log('User successfully deleted');
-    });
-  });
-};
-
-const findKennyAndDelete = function() {
-  return User.findOne({ username: 'Kenny_the_boy' }).then(function(user) {
-    return user.remove(function() {
-      console.log('User successfully deleted');
-    });
-  });
-};
-
-const findBennyAndRemove = function() {
-  return User.findOneAndRemove({ username: 'Benny_the_man' }).then(function(
-    user
-  ) {
-    return user.remove(function() {
-      console.log('User successfully deleted');
-    });
-  });
-};
-
-Promise.all([kenny.save(), mark.save(), benny.save()])
-  .then(findAllUsers)
-  .then(findSpecificRecord)
-  .then(updateUserPassword)
-  .then(updateUsername)
-  .then(findMarkAndDelete)
-  .then(findKennyAndDelete)
-  .then(findBennyAndRemove)
-  .catch(console.log.bind(console));
-
